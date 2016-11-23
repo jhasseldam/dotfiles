4c5a7d170a74512df7e70e2993f6b8f75d5a879a
Roman Kuznetsov <roman@kuznero.com>
2016-11-23 10:02:14 +0100

    Reorganize bash scripts in ~/bin

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ echo ""
 echo -e "\e[40;38;5;82m PROCESSING \e[30;48;5;82m BASH SCRIPTS \e[0m"
 echo ""
 mkdir -p $HOME/bin
-cp -v bash/*.sh $HOME/bin
+cp -rv bash/* $HOME/bin
 find bash/ -type l -exec cp -av {} $HOME/bin \;
 cp -v bash/.inputrc $HOME
 
