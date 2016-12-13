f551068399b9c06f3a5b2fe707b59ce6c446227a
Roman Kuznetsov <roman@kuznero.com>
2016-12-13 07:55:43 +0100

    Separate programming packages in nixos setup

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,15 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     # Common
-    wget curl htop tmux gparted manpages
+    wget curl htop tmux gparted manpages xclip
     # Internet
     firefox chromium hexchat irssi tdesktop
-    # Programming
-    stack texlive.combined.scheme-full
+    xe-guest-utilities
     # Gnome 3
     gnome3.gnome_keyring
     gthumb vlc
