f12e6bb04c1cf6ec0256b085697458b30ca0b3d6
Roman Kuznetsov <roman@kuznero.com>
2016-12-24 17:22:15 +0100

    Fix hosts

--- nixos/soul/networking.nix
+++ nixos/soul/networking.nix
@@ -7,8 +7,8 @@
     search = [ "kuznero.net" ];
     # fix for missing hosts entry https://github.com/NixOS/nixpkgs/issues/1248
     extraHosts = ''
-      127.0.0.1 soul.kuznero.net buddha
-      ::1 soul.kuznero.net buddha
+      127.0.0.1 soul.kuznero.net sould
+      ::1 soul.kuznero.net soul
     '';
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true;
