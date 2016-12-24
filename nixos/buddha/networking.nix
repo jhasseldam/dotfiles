856ef8d9d3d575baeb4c9bc64aeeb7323bf0f11e
Roman Kuznetsov <roman@kuznero.com>
2016-12-24 18:27:34 +0100

    minor

--- nixos/buddha/networking.nix
+++ nixos/buddha/networking.nix
@@ -12,10 +12,10 @@
     '';
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true;
-    nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
+    # nameservers = [
+    #   "8.8.8.8"
+    #   "8.8.4.4"
+    # ];
     firewall = {
       enable = true;
       allowPing = true;
--- nixos/soul/networking.nix
+++ nixos/soul/networking.nix
@@ -7,15 +7,15 @@
     search = [ "kuznero.net" ];
     # fix for missing hosts entry https://github.com/NixOS/nixpkgs/issues/1248
     extraHosts = ''
-      127.0.0.1 soul.kuznero.net sould
+      127.0.0.1 soul.kuznero.net soul
       ::1 soul.kuznero.net soul
     '';
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true;
-    nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
+    # nameservers = [
+    #   "8.8.8.8"
+    #   "8.8.4.4"
+    # ];
     firewall = {
       enable = true;
       allowPing = true;
