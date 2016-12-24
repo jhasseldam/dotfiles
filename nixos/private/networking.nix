fcafb6590e8388572f66d3e9f230db16b114b41a
Roman Kuznetsov <roman@kuznero.com>
2016-12-24 17:15:07 +0100

    Improve networking setup

--- nixos/private/networking.nix
+++ nixos/private/networking.nix
@@ -3,11 +3,24 @@
 {
   networking = {
     hostName = "buddha";
+    domain = "kuznero.net";
+    search = [ "kuznero.net" ];
+    # fix for missing hosts entry https://github.com/NixOS/nixpkgs/issues/1248
+    extraHosts = ''
+      127.0.0.1 buddha.kuznero.net buddha
+      ::1 buddha.kuznero.net buddha
+    '';
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager.enable = true;
     nameservers = [
       "8.8.8.8"
       "8.8.4.4"
     ];
+    firewall = {
+      enable = true;
+      allowPing = true;
+      allowedTCPPorts = [ 22 ];
+      allowedUDPPorts = [];
+    };
   };
 }
