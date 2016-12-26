e91b6e37614c66c808a33f572f6ac0464291d653
Roman Kuznetsov <roman@kuznero.com>
2016-12-26 21:15:29 +0100

    Use statis IPs for home stations

--- nixos/moon/networking.nix
+++ nixos/moon/networking.nix
@@ -10,6 +10,12 @@
       ::1 moon.kuznero.local moon
     '';
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    useDHCP = false;
+    interfaces = {
+      wlp2s0.ip4 = [
+        { address = "192.168.8.151"; prefixLength = 24; }
+      ];
+    };
     networkmanager.enable = true;
     nameservers = [ "192.168.8.107" ];
     firewall = {
--- nixos/soul/networking.nix
+++ nixos/soul/networking.nix
@@ -10,6 +10,12 @@
       ::1 soul.kuznero.local soul
     '';
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    useDHCP = false;
+    interfaces = {
+      wlp2s0.ip4 = [
+        { address = "192.168.8.152"; prefixLength = 24; }
+      ];
+    };
     networkmanager.enable = true;
     nameservers = [ "192.168.8.107" ];
     firewall = {
