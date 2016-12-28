ce8b2700d3345c69fc388fdc338dc60e000f5d48
Roman Kuznetsov <roman@kuznero.com>
2016-12-28 22:44:21 +0100

    Install zlibStatic as programming package

--- nixos/programming.nix
+++ nixos/programming.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     gnumake
-    stack cabal2nix graphviz
+    stack cabal2nix graphviz zlibStatic
     texlive.combined.scheme-full
     meld
     mono46 fsharp
--- nixos/soul/programming.nix
+++ nixos/soul/programming.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     gnumake
-    stack cabal2nix graphviz
+    stack cabal2nix graphviz zlibStatic
     meld
     inotify-tools
   ];
