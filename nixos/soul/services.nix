843fcf36c2b603ba98b6fe6628665668c5ba5b9c
Roman Kuznetsov <roman@kuznero.com>
2016-12-29 07:57:15 +0100

    Do not start display manager on soul

--- nixos/soul/services.nix
+++ nixos/soul/services.nix
@@ -3,13 +3,23 @@
 {
   # Enable the X11 windowing system.
   services = {
+    logind = {
+      # From: http://lists.science.uu.nl/pipermail/nix-dev/2015-July/017657.html
+      # The standard way of controlling the size of /run/user is the
+      # RuntimeDirectorySize directive
+      # in logind.conf [1]. The NixOS interface is services.logind.extraConfig.
+      # [1]: http://www.freedesktop.org/software/systemd/man/logind.conf.html
+      extraConfig = ''
+        RuntimeDirectorySize=2G
+      '';
+    };
     openssh.enable = true;
     printing = {
       enable = true;
       drivers = [ pkgs.gutenprint ];
     };
     xserver = {
-      enable = true;
+      enable = false;
       layout = "us";
       # xkbOptions = "eurosign:e";
       displayManager.gdm.enable = true;
