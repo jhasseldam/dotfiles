9a7d3656439003787c47c293a2f5fd9f5a390231
Roman Kuznetsov <roman@kuznero.com>
2017-01-01 21:55:55 +0100

    Include more xfce4 plugins

--- nixos/soul/packages.nix
+++ nixos/soul/packages.nix
@@ -10,5 +10,19 @@
     evince poppler_utils
     # Gnome 3
     gnome3.gnome_keyring
+    # Xfce 4
+    xfce.xfce4_cpufreq_plugin
+    xfce.xfce4_systemload_plugin
+    xfce.xfce4_xkb_plugin
+    xfce.xfce4_embed_plugin
+    xfce.xfce4_battery_plugin
+    xfce.xfce4_verve_plugin
+    # xfce.xfce4_notes_plugin
+    # xfce.xfce4_dict_plugin
+    xfce.xfce4_clipman_plugin
+    xfce.xfce4_datetime_plugin
+    xfce.xfce4_netload_plugin
+    xfce.gigolo
+    xfce.xfce4taskmanager
   ];
 }
