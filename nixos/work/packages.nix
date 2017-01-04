db96d98bc7ccd871b08336797d948c3152065890
Roman Kuznetsov <roman@kuznero.com>
2017-01-04 09:39:46 +0100

    Improve Xfce setup on work

--- nixos/work/packages.nix
+++ nixos/work/packages.nix
@@ -3,13 +3,26 @@
 {
   environment.systemPackages = with pkgs; [
     # Common
-    wget curl bind htop gparted manpages xclip
+    wget curl bind htop
     # Internet
-    firefox chromium
-    xe-guest-utilities
+    chromium firefox xe-guest-utilities
     # Office
     evince poppler_utils
     # Gnome 3
     gnome3.gnome_keyring
+    # Xfce 4
+    xfce.xfce4_cpufreq_plugin
+    xfce.xfce4_systemload_plugin
+    xfce.xfce4_xkb_plugin
+    xfce.xfce4_embed_plugin
+    xfce.xfce4_battery_plugin
+    xfce.xfce4_verve_plugin
+    # xfce.xfce4_notes_plugin
+    # xfce.xfce4_dict_plugin
+    xfce.xfce4_clipman_plugin
+    xfce.xfce4_datetime_plugin
+    xfce.xfce4_netload_plugin
+    xfce.gigolo
+    xfce.xfce4taskmanager
   ];
 }
