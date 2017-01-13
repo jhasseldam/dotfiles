612bab85a6333087cac2946a92812e376264b9db
Roman Kuznetsov <roman@kuznero.com>
2017-01-13 09:51:12 +0100

    Use diffuse as diff/merge tool in git

--- nixos/programming.nix
+++ nixos/programming.nix
@@ -6,7 +6,7 @@
     octaveFull
     stack cabal2nix graphviz zlibStatic
     texlive.combined.scheme-full
-    meld
+    diffuse
     python27Full
     mono46 fsharp dotnetPackages.FSharpAutoComplete dotnetPackages.FSharpCompilerCodeDom dotnetPackages.FSharpCompilerService dotnetPackages.FSharpData dotnetPackages.FSharpFormatting
     maven jdk
