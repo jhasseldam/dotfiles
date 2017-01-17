ba81948a1b5b269a73e8e83a13c9ca88516a984f
Roman Kuznetsov <roman@kuznero.com>
2017-01-17 08:27:59 +0100

    Install hexchat on soul

--- nixos/soul/packages.nix
+++ nixos/soul/packages.nix
@@ -6,7 +6,7 @@
     wget curl bind htop gksu ntfs3g
     enlightenment.terminology
     # Internet
-    chromium irssi dropbox elinks qbittorrent
+    chromium hexchat irssi dropbox elinks qbittorrent
     viber skype
     # Office
     evince poppler_utils
