a9a3169b70a9b2f769656d698c806fdfc5b5c15a
Roman Kuznetsov <roman@kuznero.com>
2017-05-13 21:54:13 +0200

    Fix stack to lts-8.13

--- nixos/devel/haskell.nix
+++ nixos/devel/haskell.nix
@@ -14,9 +14,11 @@ in
     stackSetup = {
       text = ''
         cd /home/kuznero
-        if [[ ! -d .stack ]]; then mkdir .stack; fi
+        if [[ ! -d .stack/global-project ]]; then mkdir -p .stack/global-project; fi
         if [[ -f .stack/config.yaml ]]; then rm .stack/config.yaml; fi
         ln -fs /etc/nixos/dotfiles/.stack/config.yaml .stack/config.yaml
+        if [[ -f .stack/global-project/stack.yaml ]]; then rm .stack/global-project/stack.yaml; fi
+        ln -fs /etc/nixos/dotfiles/.stack/global-project/stack.yaml .stack/global-project/stack.yaml
         chown -R kuznero:users .stack
       '';
       deps = ["users"];
