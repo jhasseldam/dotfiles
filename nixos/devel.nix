75278ba5e3bfd9e514ee30b004c42e9601ef0ba9
Roman Kuznetsov <roman@kuznero.com>
2017-05-14 20:59:27 +0200

    Always install binutils for development (strip, etc.)

--- nixos/devel.nix
+++ nixos/devel.nix
@@ -1,10 +1,16 @@
 { config, pkgs, ... }:
 
 {
+
   imports =
     [ ./devel/haskell.nix
       ./devel/dotnet.nix
       ./devel/java.nix
       ./devel/python.nix
     ];
+
+  environment.systemPackages = with pkgs; [
+    binutils
+  ];
+
 }
