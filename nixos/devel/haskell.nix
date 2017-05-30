5a8349f8dbdf03946b1593e6612fbdad9951e2eb
Roman Kuznetsov <roman@kuznero.com>
2017-05-30 07:49:04 +0200

    Stop using stack for haskell development

--- nixos/devel/haskell.nix
+++ nixos/devel/haskell.nix
@@ -1,29 +1,8 @@
 { config, pkgs, ... }:
 
-let
-  unstable = (import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {
-    inherit (config.nixpkgs) config system;
-  }).pkgs;
-in
 {
   environment.systemPackages = with pkgs; [
-    unstable.stack
+    cabalInstall
     cabal2nix
   ];
-
-  system.activationScripts = {
-    stackSetup = {
-      text = ''
-        cd /home/kuznero
-        if [[ ! -d .stack/global-project ]]; then mkdir -p .stack/global-project; fi
-        if [[ -f .stack/config.yaml ]]; then rm .stack/config.yaml; fi
-        ln -fs /etc/nixos/dotfiles/.stack/config.yaml .stack/config.yaml
-        if [[ -f .stack/global-project/stack.yaml ]]; then rm .stack/global-project/stack.yaml; fi
-        ln -fs /etc/nixos/dotfiles/.stack/global-project/stack.yaml .stack/global-project/stack.yaml
-        chown -R kuznero:users .stack
-      '';
-      deps = ["users"];
-    };
-  };
-
 }
