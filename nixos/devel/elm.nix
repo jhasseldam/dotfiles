d593d81ffe583afde599d728e9591225b1c0f454
Roman Kuznetsov <roman@kuznero.com>
2017-06-01 10:28:25 +0200

    Override elm-format with symlink to latest version

--- nixos/devel/elm.nix
+++ nixos/devel/elm.nix
@@ -1,9 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.packageOverrides = pkgs: with pkgs; {
-    elm-format = pkgs.elm-format.overrideDerivation (oldAttrs: {
-      postInstall = oldAttrs.postInstall + ''
+  nixpkgs.config.packageOverrides = pkgs: rec {
+    my-elm-format = pkgs.elmPackages.elm-format.overrideDerivation (oldAttrs: {
+      # postInstall = oldAttrs.postInstall + ''
+      postInstall = ''
         ln -s $out/bin/elm-format-0.18 $out/bin/elm-format
       '';
     });
@@ -12,7 +13,7 @@
   environment.systemPackages = with pkgs; with elmPackages; [
     elm
     elm-compiler
-    elm-format
+    my-elm-format # when `elm-format` symlink to latest version is in place, revert to `elm-format`
     elm-make
     elm-package
     elm-reactor
