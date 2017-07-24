a86d77c90528f8daed8380736e52e03c9bb20f7e
System administrator <root@nixos.dk3.danskenet.net>
2017-07-24 06:21:49 +0000

    Separate work and work-dev profiles

---
+++ profiles/work-dev/networking.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+let
+  global = (import ./global.nix);
+in
+  {
+    environment.variables.HTTP_PROXY = "http://127.0.0.1:3128/";
+    environment.variables.HTTPS_PROXY = "http://127.0.0.1:3128/";
+    networking = {
+      hostName = "soul";
+      domain = "danskenet.net";
+      search = [ "danskenet.net" ];
+      proxy.default = "http://127.0.0.1:3128/";
+      firewall.enable = false;
+      # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+      networkmanager.enable = true;
+      # nameservers = [
+      #   "8.8.8.8"
+      #   "8.8.4.4"
+      # ];
+      extraHosts =
+        if builtins.pathExists "/home/${global.userName}/.hosts"
+          then builtins.readFile "/home/${global.userName}/.hosts"
+          else "";
+    };
+    services.cntlm = {
+      enable = true;
+      username = "user";
+      password = "password";
+      domain = "domain";
+      proxy = [ "10.81.220.156:80" ];
+      extraConfig = ''
+        NoProxy *.danskenet.net docker-hub
+      '';
+    };
+  }
--- profiles/work-dev/vim/plugins.nix
+++ profiles/work-dev/vim/plugins.nix
@@ -174,10 +174,10 @@ in {
     dependencies = [];
     buildInputs = [ curl unzip ];
     buildPhase = ''
-      # export http_proxy=http://127.0.0.1:3128
-      # export https_proxy=http://127.0.0.1:3128
-      # sed -i -E "s/(.+)curl(.+)/\1curl -k\2/" Makefile
-      # make fsautocomplete
+      export http_proxy=http://127.0.0.1:3128
+      export https_proxy=http://127.0.0.1:3128
+      sed -i -E "s/(.+)curl(.+)/\1curl -k\2/" Makefile
+      make fsautocomplete
     '';
   };
 
