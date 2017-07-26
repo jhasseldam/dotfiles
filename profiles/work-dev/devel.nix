974040b3a6d38899d504daca2b6959f4b625127d
Roman Kuznetsov <roman@kuznero.com>
2017-07-26 15:47:03 +0200

    More specific dev env @ work-dev

---
+++ profiles/work-dev/devel.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports =
+    [ ./devel/dotnet.nix
+      ./devel/elm.nix
+      # ./devel/erlang.nix
+      ./devel/haskell.nix
+      # ./devel/java.nix
+      # ./devel/nodejs.nix
+      # ./devel/python.nix
+    ];
+
+  environment.systemPackages = with pkgs; [
+    gnumake
+    binutils
+    jq
+    tree
+  ];
+
+}
