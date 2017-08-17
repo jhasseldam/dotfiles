dc73fa1e526d28dadf7156ceffff199958281621
Roman Kuznetsov <roman@kuznero.com>
2017-08-17 23:18:31 +0200

    Add jupyter notebook

--- devel/python.nix
+++ devel/python.nix
@@ -3,7 +3,18 @@
 {
   environment.systemPackages = with pkgs; [
     python35Full
+    python35Packages.ipykernel
+    python35Packages.ipywidgets
     python35Packages.jupyter
+    python35Packages.jupyter_client
+    python35Packages.jupyter_console
+    python35Packages.jupyter_core
+    python35Packages.jupyterlab
+    python35Packages.nbconvert
+    python35Packages.nbformat
+    python35Packages.notebook
+    python35Packages.qtconsole
+    python35Packages.widgetsnbextension
     python35Packages.numpy
   ];
 }
