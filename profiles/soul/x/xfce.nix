ea665ddee54d66e5f1a244b8b820a7f25c5635ab
Roman Kuznetsov <roman@kuznero.com>
2017-08-21 20:27:57 +0200

    Minor

--- profiles/soul/x/xfce.nix
+++ profiles/soul/x/xfce.nix
@@ -47,7 +47,7 @@ in
         displayManager = {
           lightdm.enable = true;
           sessionCommands = ''
-            xrandr --output eDP-1 --scale 0.5x0.5
+            xrandr --output eDP-1 --scale 0.4x0.4
           '';
         };
         desktopManager.xfce = {
