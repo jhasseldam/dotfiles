556f93f62d7c4309084d9d544f01a21107663d1e
Roman Kuznetsov <roman@kuznero.com>
2017-09-12 21:49:50 +0200

    Include artifactory repository

--- profiles/work-dev/virtualization.nix
+++ profiles/work-dev/virtualization.nix
@@ -7,7 +7,7 @@
       liveRestore = false;
       logDriver = "json-file";
       extraOptions = ''
--H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 --insecure-registry docker-hub:5000 --insecure-registry docker-hub:5043 --log-opt max-size=1M --log-opt max-file=1
+-H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 --insecure-registry db-docker-fxcore.artifactory.danskenet.net --insecure-registry docker-hub:5000 --insecure-registry docker-hub:5043 --log-opt max-size=1M --log-opt max-file=1
       '';
     };
     virtualbox = {
